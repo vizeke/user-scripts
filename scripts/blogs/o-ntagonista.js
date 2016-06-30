a020372670fb2ba28b84091876239e70a6435c1f
vizeke <vizeke@gmail.com>
2016-06-30 12:58:56 -0300

    bump version

--- scripts/blogs/o-ntagonista.js
+++ scripts/blogs/o-ntagonista.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         O Antagonista - Speed Reading
 // @namespace    http://oantagonista.com/speedread
-// @version      0.4.0
+// @version      0.4.1
 // @description  Fast reading of the micro blog!
 // @author       ViZeke
 // @match        http://www.oantagonista.com/
